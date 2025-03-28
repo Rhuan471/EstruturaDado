36b9fe9b992f5a42c9e8b9075cac7d2c1d210e68
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2025-03-28 20:36:02 -0300

    Update Lidupen.java

--- Lidupen.java
+++ Lidupen.java
@@ -1,17 +1,23 @@
 package Exercicios.Vetores;
 
 public class Lidupen {
+    public static void main(String[] args) {
+        Vetor vetor=new Vetor(10);
+        Lista container = new container();
 
-    public class No {
-    
-        public int dado;
-    
-    public void Lista() {
-        this.inicio = null;
+    vetor.adicionarAoInicio(2);
+    vetor.adicionarAoInicio(7);
+    vetor.adicionarAoInicio(9);
+    vetor.adicionarAoInicio(12);
+    vetor.adicionarAoInicio(16);
+    vetor.adicionarAoInicio(21);
+    vetor.adicionarAoInicio(27);
+    vetor.adicionarAoInicio(33);
+    vetor.adicionarAoInicio(42);
+    vetor.adicionarAoInicio(54);
+
+    container.mostrarNos();
+    System.out.println("-----");
     }
-
-
-
-    }
-
 }
+
