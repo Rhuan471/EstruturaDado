ed656165a165b1d72bc2bb1b46a2db11fee1298f
Rhuan471 <127798346+Rhuan471@users.noreply.github.com>
2025-03-28 16:13:31 -0300

    Update Lidupen.java

--- Lidupen.java
+++ Lidupen.java
@@ -1,5 +1,17 @@
 package Exercicios.Vetores;
 
 public class Lidupen {
+
+    public class No {
     
+        public int dado;
+    
+    public void Lista() {
+        this.inicio = null;
+    }
+
+
+
+    }
+
 }
